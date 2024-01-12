a38b76663b5403eb14d279438aa1a14bdd638bdc
Giuseppe Boinega <giuseppe.boinega1@gmail.com>
2024-01-12 08:33:39 +0100

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,37 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL){
+
+    console.log(`crawling ${currentURL}`)
+
+    if (!currentURL.startsWith('http://') && !currentURL.startsWith('https://')) {
+        currentURL = 'https://' + currentURL;
+    }
+
+    console.log(`crawling ${currentURL}`)
+
+    try{
+
+        const resp = await fetch(currentURL)
+
+        if (resp.status > 399){
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get('content-type')
+        if (!contentType.includes('text/html')) {
+            console.log(`non html response, content-type: ${contentType} on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+
+    } catch(err){
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }    
+}
+
 function getURLsFromHTML(htmlBody, baseUrl) {
     const urls = []
     const dom = new JSDOM(htmlBody) // create a DOM object from the HTML string
@@ -49,5 +81,6 @@ function normalizeUrl(urlString) {
   
   module.exports = {
       normalizeUrl,
-      getURLsFromHTML
+      getURLsFromHTML,
+      crawlPage
   }
